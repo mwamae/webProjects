f9153f3013f2668b8ad74ca7c7a9cc8ce2a6700f
mwamae <mwamae@cytonn.com>
2015-05-20 17:16:39 +0300

    Changing Layout

--- contactApp/controller.js
+++ contactApp/controller.js
@@ -2,14 +2,15 @@ var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope){
         count = 0;
         $scope.data = [];
-        $scope.firstName = "michael";
-        $scope.lastName = "Wamae";
-        $scope.cellPhone = "0711390657";
+        $scope.data.firstName = "";
+        $scope.data.lastName = "";
+        $scope.data.cellPhone = "";
         $scope.submitForm = function(){
              $scope.data.push({firstName:$scope.firstName, lastName:$scope.lastName, cellPhone: $scope.cellPhone, index:(count++).toString()});
         };
         
         $scope.remove = function(index) {
         $scope.data.splice(index, 1);
+        
     };
 });
