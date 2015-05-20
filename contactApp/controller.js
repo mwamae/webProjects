96c2e3b1ad8bb2e7716fd277099aa05f66f21b10
mwamae <mwamae@cytonn.com>
2015-05-20 14:24:19 +0300

    add contactApp

---
+++ contactApp/controller.js
@@ -0,0 +1,16 @@
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope){
+        count = 0;
+        $scope.data = [];
+        $scope.firstName = "michael";
+        $scope.lastName = "Wamae";
+        $scope.cellPhone = "0711390657";
+        $scope.submitForm = function(){
+             $scope.data.push({firstName:$scope.firstName, lastName:$scope.lastName, cellPhone: $scope.cellPhone, index:(count++).toString()});
+        };
+        
+        $scope.remove = function(index) {
+            alert(index+" "+$scope.data.index);
+        $scope.data.splice(index, 1);
+    };
+});
